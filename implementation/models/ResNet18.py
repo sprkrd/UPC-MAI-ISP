5fd508947670bdc8218208fc59d941eecfbf887d
albertOA <alberto.olivares.alarcos@gmail.com>
2018-01-14 14:56:18 +0100

    New Playground for PGD Attacker.

--- implementation/models/ResNet18.py
+++ implementation/models/ResNet18.py
@@ -6,5 +6,6 @@ from torchvision.models import resnet18
 def pretrained_res18():
 	model = resnet18()
 	model.fc = nn.Linear(512, 4)
-	model.load_state_dict(torch.load('implementation/models/pretrained/res18_cpu.pkl'))
+	#model.load_state_dict(torch.load('implementation/models/pretrained/res18_cpu.pkl'))
+	model.load_state_dict(torch.load('models/pretrained/res18_cpu.pkl'))
 	return model
