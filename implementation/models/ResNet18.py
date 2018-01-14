4e067cfc5242eee6780f2f44d75bd890f8a51c45
Alejandro <asuarez@iri.upc.edu>
2018-01-14 15:03:49 +0100

    relative path

--- implementation/models/ResNet18.py
+++ implementation/models/ResNet18.py
@@ -1,11 +1,13 @@
 import torch
+import os
 import torch.nn as nn
 from torchvision.models import resnet18
 
 
 def pretrained_res18():
-	model = resnet18()
-	model.fc = nn.Linear(512, 4)
-	model.load_state_dict(torch.load('./pretrained/res18_cpu.pkl'))
-	return model
+    model = resnet18()
+    model.fc = nn.Linear(512, 4)
+    path = os.path.join(os.path.dirname(__file__), "pretrained", "res18_cpu.pkl")
+    model.load_state_dict(torch.load(path))
+    return model
 
