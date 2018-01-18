38d666a6e1cd29ae3b916c58f322ba1d9eab1a03
Johannes Heidecke <johannes.heidecke@sap.com>
2018-01-18 13:02:22 +0100

    gan attacks

--- implementation/attackers/BlackBoxAttacker.py
+++ implementation/attackers/BlackBoxAttacker.py
@@ -1,8 +1,11 @@
 import numpy as np
 
-from attackers.Attacker import Attacker
+from .Attacker import Attacker
 
-from models.PixelLevelTransferN import PixelLevelTransferN
+from ..models.PixelLevelTransferN import PixelLevelTransferN
+
+import torch
+from torch.autograd import Variable
 
 class BlackBoxAttacker(Attacker):
   
@@ -19,19 +22,27 @@ class BlackBoxAttacker(Attacker):
 
 class GANAttack(BlackBoxAttacker):
 
-	def __init__(self, attack_shape, intensity):
-		BlackBoxAttacker.__init__(self, attack_shape)
+    def __init__(self, attack_shape, intensity):
+        BlackBoxAttacker.__init__(self, attack_shape)
 
-		self.attacker = PixelLevelTransferN(intensity)
-
-		
-		print('Loading pre-trained GANAttack')
-		# XXX TODO
-
-
-	def attack(self, input_data):
-
-		return input_data + np.random.uniform(-self.intensity, self.intensity, self.attack_shape)
+        attacker = PixelLevelTransferN(in_channels=3, out_channels=3, intensity=0.2)
+        state_dict = torch.load('implementation/models/pretrained/gan_attacker.pkl')
+        attacker.load_state_dict(state_dict)
+        self.attacker = attacker
 
 
-	
+    def attack(self, input_data):
+
+        images = Variable(input_data, volatile=True)
+
+        return (images + self.attacker(images)).data
+
+
+if __name__ == "__main__":
+
+
+    gan_attacker = GANAttack(None, None)
+
+
+
+    
